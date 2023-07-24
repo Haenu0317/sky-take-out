953c8da6372f0bc56e9dbb67f40527fbb18ea6eb
Haenu0317 <2523053533@qq.com>
2023-07-24 23:28:36 +0800

    套餐分页查询

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,13 +1,17 @@
 package com.sky.service.impl;
 
 import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,4 +47,15 @@ public class SetmealServiceImpl implements SetmealService {
 
         setmealDishMapper.insertBatch(setmealDishes);
     }
+
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        int pageNum = setmealPageQueryDTO.getPage();
+        int pageSize = setmealPageQueryDTO.getPageSize();
+        PageHelper.startPage(pageNum,pageSize);
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+
+
 }
