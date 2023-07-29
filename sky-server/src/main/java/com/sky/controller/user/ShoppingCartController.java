8e0de8ece905b1583b230dd39dd099bdc083a421
Haenu0317 <2523053533@qq.com>
2023-07-29 19:44:12 +0800

    增加商品到购物车

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,28 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "C端-购物车相关接口")
+@Slf4j
+@RestController
+@RequestMapping("/user/shoppingCart")
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/add")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("添加购物车:{}",shoppingCartDTO);
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
+}
