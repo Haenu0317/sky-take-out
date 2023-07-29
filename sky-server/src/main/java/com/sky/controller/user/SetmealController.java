b621bf8b9ecb2e79b05a9115b62f3867f9240050
Haenu0317 <2523053533@qq.com>
2023-07-29 17:36:17 +0800

    利用Spring cache进行套餐缓存

--- sky-server/src/main/java/com/sky/controller/user/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/user/SetmealController.java
@@ -8,10 +8,12 @@ import com.sky.vo.DishItemVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController("userSetmealController")
@@ -28,6 +30,7 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
+    @Cacheable(cacheNames = "setmealCache", key = "#categoryId")
     @ApiOperation("根据分类id查询套餐")
     public Result<List<Setmeal>> list(Long categoryId) {
         Setmeal setmeal = new Setmeal();
